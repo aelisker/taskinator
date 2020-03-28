4ee2db427c652c31841b0ac1f237eee57d24f3a2
Aaron Lisker <aelisker@gmail.com>
2020-03-28 14:07:23 -0700

    revised js for submit action on form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoEd1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    console.log(event);
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoEd1.appendChild(listItemE1);
 };    
     
-buttonEl.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
 
 // console.log(buttonEl);
 
